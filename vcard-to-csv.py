b59fe0f08f96477de95504f785e1ee76fccd4fe3
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-02-17 20:55:26 -0600

    Added csv output and fixed indenting.

--- vcard-to-csv.py
+++ vcard-to-csv.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 import vobject
 import glob
+import csv
+
+with open("contacts.csv" as csv_file:
+    writer = csv.writer(csv_file, delimiter='\t')
+    writer.writerow(['Name','Telephone','Email','Note'])
+
 for file in glob.glob("*.vcf"):
+    name = ''
+    telephone = ''
+    email = ''
+    note = ''
     print file
     vCard_text = open(file).read()
     vCard = vobject.readOne(vCard_text)
     try:
-        print vCard.n.value
+        name = vCard.n.value
     except AttributeError:
         print "Could not find name"
-	pass
+        pass
     try:
-        print vCard.tel.value
+        telelphone = vCard.tel.value
     except AttributeError:
         print "Could not find telephone number"
-	pass
+        pass
     try:
-        print vCard.email.value
+        email = vCard.email.value
     except AttributeError:
         pass
     try:
-        print vCard.note.value
+        note = print vCard.note.value
     except AttributeError:
         pass
+    write.writerow([name, telephone, email, note])
+
 else: # Last for loop iteration
     import code
     code.interact(local=locals())
