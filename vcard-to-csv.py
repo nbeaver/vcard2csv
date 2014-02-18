67f3ccdd0794b41dc9be57a01ba9904fc531d9c3
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-02-17 20:42:04 -0600

    Exception handling.

--- vcard-to-csv.py
+++ vcard-to-csv.py
@@ -5,10 +5,24 @@ for file in glob.glob("*.vcf"):
     print file
     s = open(file).read()
     v = vobject.readOne(s)
-    print v.tel.value
-    print v.tel.value
-    print v.email.value
-    print v.note.value
-else:
+    try:
+        print v.n.value
+    except AttributeError:
+        print "Could not find name"
+	pass
+    try:
+        print v.tel.value
+    except AttributeError:
+        print "Could not find telephone number"
+	pass
+    try:
+        print v.email.value
+    except AttributeError:
+        pass
+    try:
+        print v.note.value
+    except AttributeError:
+        pass
+else: # Last for loop iteration
     import code
     code.interact(local=locals())
