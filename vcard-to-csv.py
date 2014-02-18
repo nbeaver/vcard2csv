fc0054a485fd309940237abc5a69727c2dd87630
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-02-17 20:34:56 -0600

    Enhanced debugging and printing actual values.

--- vcard-to-csv.py
+++ vcard-to-csv.py
@@ -1,8 +1,14 @@
-# coding: utf-8
+#!/usr/bin/env python
 import vobject
 import glob
 for file in glob.glob("*.vcf"):
     print file
     s = open(file).read()
     v = vobject.readOne(s)
-    print v.prettyPrint()
+    try print v.tel.value
+    try print v.tel.value
+    try print v.email.value
+    try print v.note.value
+else:
+    import code
+    code.interact(local=locals())
