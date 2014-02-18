475cb95eff68c3ddb41787d5bc60e3b70068eec5
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-02-18 00:31:20 -0600

    Added handling for multiple phone number (cell, work, home).

--- vcard-to-csv.py
+++ vcard-to-csv.py
@@ -6,10 +6,10 @@ import csv
 #TODO It might be a good idea to make the output file configurable
 csv_file = open("contacts.csv", 'w')
 writer = csv.writer(csv_file, delimiter='\t') # Technically a tab-delimited file
-writer.writerow(['Name','Telephone','Email','Note'])
+writer.writerow(['Name','Cell phone','Work phone','Home phone','Email','Note'])
 
 for file in sorted(glob.glob("*.vcf")):
-    name = telephone = email = note = ''
+    name = cell = work = home = email = note = ''
     print file
     vCard_text = open(file).read()
     vCard = vobject.readOne(vCard_text)
@@ -21,7 +21,17 @@ for file in sorted(glob.glob("*.vcf")):
         # A vCard without a name is not good.
         # It does not get a pass.
     try:
-        telephone = str(vCard.tel.value).strip()
+        for tel in vCard.tel_list:
+            # This strategy limits this to version 2.1 vCards.
+            # We would need e.g. type=CELL for version 3.0 vCards.
+            if 'CELL' in tel.singletonparams:
+                cell = str(tel.value).strip()
+            elif 'WORK' in tel.singletonparams:
+                work = str(tel.value).strip()
+            elif 'HOME' in tel.singletonparams:
+                home = str(tel.value).strip()
+            else:
+                print "Unrecognized phone number category:",str(tel.singletonparams),' for phone number ',tel.value, tel
     except AttributeError:
         print "Could not find telephone number for file ",file
         pass # Missing phone number is worth mentioning, but not stopping for.
@@ -33,7 +43,7 @@ for file in sorted(glob.glob("*.vcf")):
         note = str(vCard.note.value)
     except AttributeError:
         pass # Many vCards will not have a note.
-    writer.writerow([name, telephone, email, note])
+    writer.writerow([name, cell, work, home, email, note])
 #else: # This is executed after the last run of the for loop
 #    import code
 #    code.interact(local=locals())
