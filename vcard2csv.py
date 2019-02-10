0fe80e18836478df96897fd3f307f3053e27c6fa
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-02-10 17:48:24 -0600

    Use argparse.

--- vcard2csv.py
+++ vcard2csv.py
@@ -2,7 +2,8 @@
 import vobject # to parse vCard (vcf) files
 import glob # to open all *.vcf files
 import csv
-import sys
+import argparse
+import os.path
 
 def get_phone_numbers(vCard):
     cell = home = work = None
@@ -59,25 +60,41 @@ def get_info_list(file):
 
     return [name, cell, work, home, email, note]
 
+def readable_directory(path):
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(
+            'not an existing directory: {}'.format(path))
+    if not os.access(path, os.R_OK):
+        raise argparse.ArgumentTypeError(
+            'not a readable directory: {}'.format(path))
+    return path
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Combine a bunch of vCard (.vcf) files to TSV.'
+    )
+    parser.add_argument(
+        'read_dir',
+        type=readable_directory,
+        help='Directory to read vCard files from.'
+    )
+    parser.add_argument(
+        'tsv_file',
+        type=argparse.FileType('w'),
+        help='Output file',
+    )
+    args = parser.parse_args()
+    vcard_pattern = os.path.join(args.read_dir, "*.vcf")
 
-    try:
-        output_file_name = sys.argv[1]
-    except IndexError:
-        print "Usage: vcard-to-csv.py foo.csv"
-        sys.exit(1)
-
-    vcards = sorted(glob.glob("*.vcf"))
+    vcards = sorted(glob.glob(vcard_pattern))
 
     if len(vcards) == 0:
-        print "Error: no files ending with `.vcf` in current directory."
+        print "Error: no files ending with `.vcf` in directory `{}'".format(args.readable_directory)
         sys.exit(2)
 
-    csv_file = open(output_file_name, 'w')
-
     # Tab separated values are less annoying than comma-separated values.
-    writer = csv.writer(csv_file, delimiter='\t')
+    writer = csv.writer(args.tsv_file, delimiter='\t')
     writer.writerow(['Name','Cell phone','Work phone','Home phone','Email','Note'])
 
-    for file in vcards:
-        writer.writerow(get_info_list(file))
+    for vcard in vcards:
+        writer.writerow(get_info_list(vcard))
