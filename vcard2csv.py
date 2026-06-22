dc310e877b94a9f50651740efd72e9ee13802b64
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2026-06-22 16:03:44 -0400

    Automatically format with black.

--- vcard2csv.py
+++ vcard2csv.py
@@ -7,32 +7,37 @@ import os.path
 import sys
 import logging
 import collections
+
 logger = logging.getLogger(__name__)
 
 column_order = [
-    'Formatted Name',
-    'Name',
-    'Prefix',
-    'Given name',
-    'Additional name',
-    'Family name',
-    'Suffix',
-    'Telephone',
-    'Home phone',
-    'Cell phone',
-    'Mobile phone',
-    'Work phone',
-    'Preferred phone',
-    'Email',
-    'Address',
-    'Note',
-    'Birthday',
+    "Formatted Name",
+    "Name",
+    "Prefix",
+    "Given name",
+    "Additional name",
+    "Family name",
+    "Suffix",
+    "Telephone",
+    "Home phone",
+    "Cell phone",
+    "Mobile phone",
+    "Work phone",
+    "Preferred phone",
+    "Email",
+    "Address",
+    "Note",
+    "Birthday",
 ]
 
+
 class PhoneNumbers:
-    fields = ('cell', 'home', 'mobile', 'phone', 'preferred', 'work')
+    fields = ("cell", "home", "mobile", "phone", "preferred", "work")
     __slots__ = fields
-    def __init__(self, cell=None, home=None, mobile=None, phone=None, preferred=None, work=None):
+
+    def __init__(
+        self, cell=None, home=None, mobile=None, phone=None, preferred=None, work=None
+    ):
         self.cell = cell
         self.home = home
         self.mobile = mobile
@@ -41,62 +46,78 @@ class PhoneNumbers:
         self.work = work
 
     def __repr__(self):
-        args_repr = ["{}={}".format(attr, repr(getattr(self, attr))) for attr in self.fields]
-        self_repr = self.__class__.__name__ + '({})'.format(', '.join(args_repr))
+        args_repr = [
+            "{}={}".format(attr, repr(getattr(self, attr))) for attr in self.fields
+        ]
+        self_repr = self.__class__.__name__ + "({})".format(", ".join(args_repr))
         return self_repr
 
     def __str__(self):
-        args_str = ["{}={}".format(attr, repr(getattr(self, attr))) for attr in self.fields]
-        self_str = self.__class__.__name__ + '({})'.format(', '.join(args_str))
+        args_str = [
+            "{}={}".format(attr, repr(getattr(self, attr))) for attr in self.fields
+        ]
+        self_str = self.__class__.__name__ + "({})".format(", ".join(args_str))
         return self_str
+
 
 def get_phone_numbers(vCard):
     phone_numbers = PhoneNumbers()
     for tel in vCard.tel_list:
-        if vCard.version.value == '2.1':
+        if vCard.version.value == "2.1":
             # tel.value should already be a string.
             tel_value = tel.value.strip()
             params = [param.lower() for param in tel.singletonparams]
             if params == []:
                 phone_numbers.phone = tel_value
-            elif 'cell' in params:
+            elif "cell" in params:
                 phone_numbers.cell = tel_value
-            elif 'work' in params:
+            elif "work" in params:
                 phone_numbers.work = tel_value
-            elif 'home' in params:
+            elif "home" in params:
                 phone_numbers.home = tel_value
-            elif 'mobile' in params:
+            elif "mobile" in params:
                 phone_numbers.mobile = tel_value
-            elif 'pref' in params:
+            elif "pref" in params:
                 phone_numbers.preferred = tel_value
             else:
-                logger.warning("Warning: Unrecognized phone number category in {}".format(repr(vCard.tel_list)))
+                logger.warning(
+                    "Warning: Unrecognized phone number category in {}".format(
+                        repr(vCard.tel_list)
+                    )
+                )
                 logger.info("tel = {}".format(tel.prettyPrint()))
-        elif vCard.version.value == '3.0':
+        elif vCard.version.value == "3.0":
             # tel.value should already be a string.
             tel_value = tel.value.strip()
-            if 'TYPE' in tel.params:
-                telephone_type = [val.lower() for val in tel.params['TYPE']]
-                if 'cell' in telephone_type:
+            if "TYPE" in tel.params:
+                telephone_type = [val.lower() for val in tel.params["TYPE"]]
+                if "cell" in telephone_type:
                     phone_numbers.cell = tel_value
-                elif 'work' in telephone_type:
+                elif "work" in telephone_type:
                     phone_numbers.work = tel_value
-                elif 'home' in telephone_type:
+                elif "home" in telephone_type:
                     phone_numbers.home = tel_value
-                elif 'mobile' in telephone_type:
+                elif "mobile" in telephone_type:
                     phone_numbers.mobile = tel_value
-                elif 'pref' in telephone_type:
+                elif "pref" in telephone_type:
                     phone_numbers.preferred = tel_value
                 else:
-                    logger.warning("Unrecognized phone number category in `{}'".format(repr(vCard.tel_list)))
+                    logger.warning(
+                        "Unrecognized phone number category in `{}'".format(
+                            repr(vCard.tel_list)
+                        )
+                    )
                     tel.prettyPrint()
             else:
                 phone_numbers.phone = tel_value
         else:
-            raise NotImplementedError("Version not implemented: {}".format(vCard.version.value))
+            raise NotImplementedError(
+                "Version not implemented: {}".format(vCard.version.value)
+            )
 
     logger.debug("phone_numbers = {}".format(phone_numbers))
     return phone_numbers
+
 
 def get_info_list(vCard, vcard_filepath):
     vcard = collections.OrderedDict()
@@ -105,56 +126,63 @@ def get_info_list(vCard, vcard_filepath):
     name = cell = work = home = mobile = None
     vCard.validate()
     for key, val in list(vCard.contents.items()):
-        if key == 'fn':
+        if key == "fn":
             logger.debug("fn = {}".format(repr(vCard.fn.value)))
-            vcard['Formatted Name'] = vCard.fn.value
-        elif key == 'n':
+            vcard["Formatted Name"] = vCard.fn.value
+        elif key == "n":
             logger.debug("n = {}".format(repr(vCard.n.value)))
-            vcard['Name'] = str(vCard.n.value).strip()
-            vcard['Prefix'] = vCard.n.value.prefix
-            vcard['Given name'] = vCard.n.value.given
-            vcard['Additional name'] = vCard.n.value.additional
-            vcard['Family name'] = vCard.n.value.family
-            vcard['Suffix'] = vCard.n.value.suffix
-        elif key == 'tel':
+            vcard["Name"] = str(vCard.n.value).strip()
+            vcard["Prefix"] = vCard.n.value.prefix
+            vcard["Given name"] = vCard.n.value.given
+            vcard["Additional name"] = vCard.n.value.additional
+            vcard["Family name"] = vCard.n.value.family
+            vcard["Suffix"] = vCard.n.value.suffix
+        elif key == "tel":
             logger.debug("tel_list = {}".format(repr(vCard.tel_list)))
             phone_numbers = get_phone_numbers(vCard)
-            vcard['Telephone'] = phone_numbers.phone
-            vcard['Cell phone'] = phone_numbers.cell
-            vcard['Home phone'] = phone_numbers.home
-            vcard['Work phone'] = phone_numbers.work
-            vcard['Mobile phone'] = phone_numbers.mobile
-            vcard['Preferred phone'] = phone_numbers.preferred
-        elif key == 'email':
+            vcard["Telephone"] = phone_numbers.phone
+            vcard["Cell phone"] = phone_numbers.cell
+            vcard["Home phone"] = phone_numbers.home
+            vcard["Work phone"] = phone_numbers.work
+            vcard["Mobile phone"] = phone_numbers.mobile
+            vcard["Preferred phone"] = phone_numbers.preferred
+        elif key == "email":
             logger.debug("email = {}".format(repr(vCard.email.value)))
             email = str(vCard.email.value).strip()
-            vcard['Email'] = email
-        elif key == 'note':
+            vcard["Email"] = email
+        elif key == "note":
             logger.debug("note = {}".format(repr(vCard.note.value)))
             note = str(vCard.note.value)
-            vcard['Note'] = note
-        elif key == 'adr':
+            vcard["Note"] = note
+        elif key == "adr":
             logger.debug("adr = {}".format(repr(vCard.adr.value)))
             adr = str(vCard.adr.value).strip()
             if adr.startswith('"') and adr.endswith('"'):
                 adr = adr[1:-1]
-            vcard['Address'] = adr
-        elif key == 'bday':
+            vcard["Address"] = adr
+        elif key == "bday":
             bday = str(vCard.bday.value).strip()
-            vcard['Birthday'] = bday
-        elif key == 'version':
+            vcard["Birthday"] = bday
+        elif key == "version":
             # Ignore the key for vcard version
             pass
         else:
-            logging.warning('unused key {} in file {}'.format(repr(key), repr(vcard_filepath)))
+            logging.warning(
+                "unused key {} in file {}".format(repr(key), repr(vcard_filepath))
+            )
             # An unused key, like `adr`, `title`, `url`, etc.
             pass
     if name is None:
         logger.warning("no name for vCard in file `{}'".format(vcard_filepath))
     if all(telephone_number is None for telephone_number in [cell, work, home, mobile]):
-        logger.warning("no telephone numbers for file `{}' with name `{}'".format(vcard_filepath, name))
+        logger.warning(
+            "no telephone numbers for file `{}' with name `{}'".format(
+                vcard_filepath, name
+            )
+        )
 
     return vcard
+
 
 def get_vcards(vcard_filepath):
     with open(vcard_filepath) as fp:
@@ -165,63 +193,60 @@ def get_vcards(vcard_filepath):
 
 def readable_directory(path):
     if not os.path.isdir(path):
-        raise argparse.ArgumentTypeError(
-            'not an existing directory: {}'.format(path))
+        raise argparse.ArgumentTypeError("not an existing directory: {}".format(path))
     if not os.access(path, os.R_OK):
-        raise argparse.ArgumentTypeError(
-            'not a readable directory: {}'.format(path))
+        raise argparse.ArgumentTypeError("not a readable directory: {}".format(path))
     return path
+
 
 def writable_file(path):
     if os.path.exists(path):
         if not os.access(path, os.W_OK):
-            raise argparse.ArgumentTypeError(
-                'not a writable file: {}'.format(path))
+            raise argparse.ArgumentTypeError("not a writable file: {}".format(path))
     else:
         # If the file doesn't already exist,
         # the most direct way to tell if it's writable
         # is to try writing to it.
-        with open(path, 'w') as fp:
+        with open(path, "w") as fp:
             pass
     return path
 
+
 def main():
     parser = argparse.ArgumentParser(
-        description='Convert a bunch of vCard (.vcf) files to a single TSV file.'
+        description="Convert a bunch of vCard (.vcf) files to a single TSV file."
     )
     parser.add_argument(
-        'read_dir',
-        type=readable_directory,
-        help='Directory to read vCard files from.'
+        "read_dir", type=readable_directory, help="Directory to read vCard files from."
     )
     parser.add_argument(
-        'tsv_file',
+        "tsv_file",
         type=writable_file,
-        help='Output file',
+        help="Output file",
     )
     parser.add_argument(
-        '-v',
-        '--verbose',
-        help='More verbose logging',
+        "-v",
+        "--verbose",
+        help="More verbose logging",
         dest="loglevel",
         default=logging.WARNING,
         action="store_const",
         const=logging.INFO,
     )
     parser.add_argument(
-        '-d',
-        '--debug',
-        help='Enable debugging logs',
+        "-d",
+        "--debug",
+        help="Enable debugging logs",
         action="store_const",
         dest="loglevel",
         const=logging.DEBUG,
     )
     parser.add_argument(
-        '-r',
-        '--recursive',
-        help='Recursively search for vcard files in the specified directory & subdirectories',
-        action='store_true',
-        dest='is_recursive'
+        "-r",
+        "--recursive",
+        help="Recursively search for vcard files in the specified directory & subdirectories",
+        action="store_true",
+        dest="is_recursive",
     )
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
@@ -233,12 +258,14 @@ def main():
         vcard_pattern = os.path.join(args.read_dir, "*.vcf")
     vcard_paths = sorted(glob.glob(vcard_pattern, recursive=args.is_recursive))
     if len(vcard_paths) == 0:
-        logger.error("no files ending with `.vcf` in directory `{}'".format(args.read_dir))
+        logger.error(
+            "no files ending with `.vcf` in directory `{}'".format(args.read_dir)
+        )
         raise FileNotFoundError
 
     # Tab separated values are less annoying than comma-separated values.
-    with open(args.tsv_file, 'w', encoding="utf-8", newline='') as tsv_fp:
-        writer = csv.writer(tsv_fp, delimiter='\t')
+    with open(args.tsv_file, "w", encoding="utf-8", newline="") as tsv_fp:
+        writer = csv.writer(tsv_fp, delimiter="\t")
         writer.writerow(column_order)
 
         for vcard_path in vcard_paths:
@@ -246,6 +273,7 @@ def main():
             for vcard in get_vcards(vcard_path):
                 vcard_info = get_info_list(vcard, vcard_path)
                 writer.writerow(list(vcard_info.values()))
+
 
 if __name__ == "__main__":
     main()
